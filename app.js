29eda5c2669435f5e91f0f0100f28b86935650ad
ViPhong <viphongly2804@gmail.com>
2025-01-30 08:34:46 +0700

    Add test view route to app.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require("./db");
 
-
+const testViewRoute = require('./routes/testView')
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use("/test/views", testViewRoute)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
